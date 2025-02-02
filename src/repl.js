9487505f1e30ab3c537eb207408ac39e83e55777
rolitoxdd <rolo.alv97@gmail.com>
2025-02-02 19:02:12 -0300

    repl

--- src/repl.js
+++ src/repl.js
@@ -2,8 +2,29 @@ const { prompt } = require('inquirer');
 const chalk = require('chalk');
 
 const { parseAndEvaluate } = require('./parse-and-evaluate');
+const { parse } = require('@babel/core');
 
-const repl = async () => {};
+const ask = () => {
+  const questions = [
+    {
+      name: 'COMMAND',
+      type: 'input',
+      message: chalk.blue('>>'),
+    },
+  ];
+  return prompt(questions);
+};
+
+const repl = async () => {
+  while (true) {
+    const res = await ask();
+    try {
+      console.log(parseAndEvaluate(res.COMMAND));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
 
 if (require.main === module) {
   console.log(
